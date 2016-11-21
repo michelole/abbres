537390d3af278cca6d8d0e3552746eacd1093473
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-11-21 15:50:19 +0100

    Adds support for concatenating strings

--- src/main/java/at/medunigraz/imi/abbres/model/mapper/AbstractMapper.java
+++ src/main/java/at/medunigraz/imi/abbres/model/mapper/AbstractMapper.java
@@ -3,6 +3,7 @@ package at.medunigraz.imi.abbres.model.mapper;
 public abstract class AbstractMapper implements Mapper {
 
 	private static final String ABBREVIATION_MARKER = ".";
+	private static final String TOKEN_SEPARATOR = " ";
 
 	/**
 	 * Removes abbreviation out of one string.
@@ -12,6 +13,17 @@ public abstract class AbstractMapper implements Mapper {
 	 */
 	protected String trimAbbreviation(String abbreviation) {
 		return abbreviation.substring(0, abbreviation.indexOf(ABBREVIATION_MARKER));
+	}
+
+	/**
+	 * Concatenates two strings using the default token separator.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	protected String concatenate(String a, String b) {
+		return String.join(TOKEN_SEPARATOR, a, b);
 	}
 
 }
