e7e3427de2bb213e1fa4ef868d960efdbd661f69
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-11-24 19:23:16 +0100

    Implements a reader for validation/test sets

---
+++ src/main/java/at/medunigraz/imi/abbres/dao/ValidationReader.java
@@ -0,0 +1,85 @@
+package at.medunigraz.imi.abbres.dao;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import at.medunigraz.imi.abbres.model.Abbreviation;
+import at.medunigraz.imi.abbres.model.context.LeftContext;
+import at.medunigraz.imi.abbres.model.context.RightContext;
+
+public class ValidationReader implements Closeable, Iterator<Abbreviation> {
+	private File file;
+
+	private static final String OUT_OF_SCOPE = "OUT OF SCOPE";
+	private static final char ABBREVIATION_MARK = '.';
+	private static final String TOKEN_SEPARATOR = " ";
+
+	private static final int WINDOW_SIZE = 100;
+
+	private XSSFWorkbook workbook;
+
+	private Iterator<Row> rowIterator;
+
+	public ValidationReader(File file) {
+		this.file = file;
+		openSheet();
+	}
+
+	private void openSheet() {
+		try {
+			workbook = new XSSFWorkbook(new FileInputStream(file));
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			rowIterator = sheet.iterator();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		workbook.close();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return rowIterator.hasNext();
+	}
+
+	@Override
+	public Abbreviation next() {
+		Row row = rowIterator.next();
+
+		String expansion = row.getCell(1).getStringCellValue();
+
+		if (expansion.isEmpty()) {
+			return null;
+		}
+
+		if (expansion.equals(OUT_OF_SCOPE)) {
+			return null;
+		}
+
+		String sourceText = row.getCell(0).getStringCellValue();
+
+		// Sanity check
+		if (sourceText.charAt(WINDOW_SIZE / 2) != ABBREVIATION_MARK) {
+			return null;
+		}
+
+		int lastSeparator = sourceText.lastIndexOf(TOKEN_SEPARATOR);
+		String token = sourceText.substring(lastSeparator + 1, WINDOW_SIZE / 2);
+
+		LeftContext leftContext = new LeftContext(sourceText.substring(0, lastSeparator));
+		RightContext rightContext = new RightContext(sourceText.substring(WINDOW_SIZE / 2 + 1, sourceText.length()));
+
+		return new Abbreviation(token).withExpansion(expansion).withLeftContext(leftContext)
+				.withRightContext(rightContext);
+	}
+}
