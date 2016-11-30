8bb23ec57d5586456ba35909809b6c7a40f95600
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-11-30 19:20:44 +0100

    Remove deprecated method and its calls

--- src/main/java/at/medunigraz/imi/abbres/resolver/DefaultResolver.java
+++ src/main/java/at/medunigraz/imi/abbres/resolver/DefaultResolver.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 import at.medunigraz.imi.abbres.model.Abbreviation;
 import at.medunigraz.imi.abbres.model.NGramMapFactory;
-import at.medunigraz.imi.abbres.model.context.LeftContext;
-import at.medunigraz.imi.abbres.model.context.RightContext;
 import at.medunigraz.imi.abbres.model.mapper.LeftBigramMapper;
 import at.medunigraz.imi.abbres.model.mapper.Mapper;
 import at.medunigraz.imi.abbres.model.mapper.RightBigramMapper;
@@ -25,11 +23,4 @@ public class DefaultResolver implements Resolver {
 		Mapper rightBigram = new RightBigramMapper(abbreviation);
 		return new BigramWithFallbackReducer().reduce(Arrays.asList(unigram, leftBigram, rightBigram));
 	}
-
-	@Deprecated
-	public String resolve(String abbreviation, String leftContext, String rightContext) {
-		return resolve(new Abbreviation(abbreviation).withLeftContext(new LeftContext(leftContext))
-				.withRightContext(new RightContext(rightContext)));
-	}
-
 }
--- src/test/java/at/medunigraz/imi/abbres/resolver/DefaultResolverTest.java
+++ src/test/java/at/medunigraz/imi/abbres/resolver/DefaultResolverTest.java
@@ -1,15 +1,11 @@
 package at.medunigraz.imi.abbres.resolver;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import at.medunigraz.imi.abbres.model.Abbreviation;
 import at.medunigraz.imi.abbres.model.NGramMap;
 import at.medunigraz.imi.abbres.model.NGramMapFactory;
-import at.medunigraz.imi.abbres.model.mapper.Mapper;
-import at.medunigraz.imi.abbres.model.mapper.UnigramMapper;
-import at.medunigraz.imi.abbres.model.reducer.BigramWithFallbackReducer;
 import junit.framework.TestCase;
 
 public class DefaultResolverTest extends TestCase {
@@ -23,7 +19,7 @@ public class DefaultResolverTest extends TestCase {
 		unigramMap.put("Pr", 10);
 		unigramMap.put("Prid.", 10);
 		NGramMapFactory.setUnigramMap(new NGramMap(unigramMap));
-		
+
 		Map<String, Integer> bigramMap = new TreeMap<>();
 		bigramMap.put("Pride and", 10);
 		bigramMap.put("and Prejudice", 1000);
@@ -33,8 +29,9 @@ public class DefaultResolverTest extends TestCase {
 		bigramMap.put("and Pr", 10);
 		bigramMap.put("and Prej.", 10);
 		NGramMapFactory.setBigramMap(new NGramMap(bigramMap));
-		
-		String actual = new DefaultResolver().resolve("Pr.", "and", "is");
+
+		Abbreviation a = new Abbreviation("Pr.").withLeftContext("and").withRightContext("is");
+		String actual = new DefaultResolver().resolve(a);
 		assertEquals("Prejudice", actual);
 	}
 }
