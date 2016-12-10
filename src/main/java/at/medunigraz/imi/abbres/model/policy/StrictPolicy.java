fd3ecee8a61b3b84d10954686cdeb44403884037
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-12-10 16:47:27 +0100

    Refactors compareTo from Mapper to Policy

--- src/main/java/at/medunigraz/imi/abbres/model/policy/StrictPolicy.java
+++ src/main/java/at/medunigraz/imi/abbres/model/policy/StrictPolicy.java
@@ -4,7 +4,31 @@ public class StrictPolicy extends AbstractPolicy {
 
 	@Override
 	public int compareTo(Policy o) {
-		// TODO Auto-generated method stub
+		// (1) Strict match with bigrams
+		// (2) Fuzzy match (abc -> a.*b.*c.*) with bigrams
+		// (3) Strict match with unigrams
+		// (4) Fuzzy match (abc -> a.*b.*c.*) with unigrams
+		int matcherComparison = this.getMatcher().compareTo(o.getMatcher());
+		if (o instanceof FuzzyPolicy) {
+			// Strict Unigram < Fuzzy Bigram
+			if (matcherComparison < 0) {
+				return -1;
+			}
+			// Strict Unigram > Fuzzy Unigram
+			// Strict Bigram > Fuzzy Unigram
+			// Strict Bigram > Fuzzy Bigram
+			return +1;
+		}
+		if (o instanceof StrictPolicy) {
+			// Strict Bigram == Strict Bigram
+			// Strict Unigram == Strict Unigram
+//			if (matcherComparison == 0) {
+//				return this.getBestEntry().getValue() > o.getBestEntry().getValue() ? +1 : -1;
+//			}
+			// Strict Unigram < Strict Bigram
+			// Strict Bigram > Strict Unigram
+			return matcherComparison;
+		}
 		return 0;
 	}
 
