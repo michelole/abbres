045e861fce3a056bb9f90e0be3b6e1d9c8f57cb1
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-12-10 19:14:58 +0100

    Remove overrided methods to force use of parent ones

--- src/main/java/at/medunigraz/imi/abbres/model/mapper/FuzzyMapper.java
+++ src/main/java/at/medunigraz/imi/abbres/model/mapper/FuzzyMapper.java
@@ -9,47 +9,6 @@ public class FuzzyMapper extends AbstractMapper {
 		super(matcher, new FuzzyPolicy());
 	}
 
-	@Deprecated
-	public String prefix() {
-		String token = matcher.getAbbreviation().getToken();
-		if (token.isEmpty()) {
-			return "";
-		}
-
-		String firstCharAbbrev = token.substring(0, 1);
-		return matcher.prefix().concat(firstCharAbbrev);
-	}
-
-	@Override
-	public int compareTo(Mapper o) {
-		// (1) Strict match with bigrams
-		// (2) Fuzzy match (abc -> a.*b.*c.*) with bigrams
-		// (3) Strict match with unigrams
-		// (4) Fuzzy match (abc -> a.*b.*c.*) with unigrams
-		int matcherComparison = this.getMatcher().compareTo(o.getMatcher());
-		if (o instanceof StrictMapper) {
-			// Fuzzy Bigram > Strict Unigram
-			if (matcherComparison > 0) {
-				return +1;
-			}
-			// Fuzzy Bigram < Strict Bigram
-			// Fuzzy Unigram < Strict Unigram
-			// Fuzzy Unigram < Strict Bigram
-			return -1;
-		}
-		if (o instanceof FuzzyMapper) {
-			// Fuzzy Bigram == Fuzzy Bigram
-			// Fuzzy Unigram == Fuzzy Unigram
-			if (matcherComparison == 0) {
-				return this.getBestEntry().getValue() > o.getBestEntry().getValue() ? +1 : -1;
-			}
-			// Fuzzy Unigram < Fuzzy Bigram
-			// Fuzzy Bigram > Fuzzy Unigram
-			return matcherComparison;
-		}
-		return 0;
-	}
-
 	@Override
 	public int getPriority() {
 		// TODO Auto-generated method stub
--- src/main/java/at/medunigraz/imi/abbres/model/mapper/StrictMapper.java
+++ src/main/java/at/medunigraz/imi/abbres/model/mapper/StrictMapper.java
@@ -9,42 +9,6 @@ public class StrictMapper extends AbstractMapper {
 		super(matcher, new StrictPolicy());
 	}
 
-	@Deprecated
-	public String prefix() {
-		String trimmedAbbrev = matcher.getAbbreviation().getTrimmedToken();
-		return matcher.prefix().concat(trimmedAbbrev);
-	}
-
-	@Override
-	public int compareTo(Mapper o) {
-		// (1) Strict match with bigrams
-		// (2) Fuzzy match (abc -> a.*b.*c.*) with bigrams
-		// (3) Strict match with unigrams
-		// (4) Fuzzy match (abc -> a.*b.*c.*) with unigrams
-		int matcherComparison = this.getMatcher().compareTo(o.getMatcher());
-		if (o instanceof FuzzyMapper) {
-			// Strict Unigram < Fuzzy Bigram
-			if (matcherComparison < 0) {
-				return -1;
-			}
-			// Strict Unigram > Fuzzy Unigram
-			// Strict Bigram > Fuzzy Unigram
-			// Strict Bigram > Fuzzy Bigram
-			return +1;
-		}
-		if (o instanceof StrictMapper) {
-			// Strict Bigram == Strict Bigram
-			// Strict Unigram == Strict Unigram
-			if (matcherComparison == 0) {
-				return this.getBestEntry().getValue() > o.getBestEntry().getValue() ? +1 : -1;
-			}
-			// Strict Unigram < Strict Bigram
-			// Strict Bigram > Strict Unigram
-			return matcherComparison;
-		}
-		return 0;
-	}
-
 	@Override
 	public int getPriority() {
 		// TODO Auto-generated method stub
