0e4241341d7e07a55f5d4d17e77c6b435ff554c1
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-12-11 03:34:06 +0100

    Adds DoubleMapper to the DefaultResolver

--- src/main/java/at/medunigraz/imi/abbres/resolver/DefaultResolver.java
+++ src/main/java/at/medunigraz/imi/abbres/resolver/DefaultResolver.java
@@ -5,6 +5,7 @@ import java.util.TreeSet;
 
 import at.medunigraz.imi.abbres.model.Abbreviation;
 import at.medunigraz.imi.abbres.model.NGramMapFactory;
+import at.medunigraz.imi.abbres.model.mapper.DoubleMapper;
 import at.medunigraz.imi.abbres.model.mapper.Mapper;
 import at.medunigraz.imi.abbres.model.mapper.SingleMapper;
 import at.medunigraz.imi.abbres.model.matcher.LeftBigramMatcher;
@@ -27,24 +28,33 @@ public class DefaultResolver implements Resolver {
 		Matcher unigram = new UnigramMatcher(abbreviation);
 		Matcher leftBigram = new LeftBigramMatcher(abbreviation);
 		Matcher rightBigram = new RightBigramMatcher(abbreviation);
-		
+
 		Policy strict = new StrictPolicy();
 		Mapper singleStrictUnigram = new SingleMapper(unigram, strict);
 		Mapper singleStrictLeftBigram = new SingleMapper(leftBigram, strict);
 		Mapper singleStrictRightBigram = new SingleMapper(rightBigram, strict);
-		
+		Mapper doubleStrictLeftBigram = new DoubleMapper(leftBigram, strict);
+		Mapper doubleStrictRightBigram = new DoubleMapper(rightBigram, strict);
+
 		Policy fuzzy = new FuzzyPolicy();
 		Mapper singleFuzzyUnigram = new SingleMapper(unigram, fuzzy);
 		Mapper singleFuzzyLeftBigram = new SingleMapper(leftBigram, fuzzy);
 		Mapper singleFuzzyRightBigram = new SingleMapper(rightBigram, fuzzy);
+		Mapper doubleFuzzyLeftBigram = new DoubleMapper(leftBigram, fuzzy);
+		Mapper doubleFuzzyRightBigram = new DoubleMapper(rightBigram, fuzzy);
 
 		NavigableSet<Mapper> set = new TreeSet<>();
 		set.add(singleStrictUnigram);
 		set.add(singleStrictLeftBigram);
 		set.add(singleStrictRightBigram);
+		set.add(doubleStrictLeftBigram);
+		set.add(doubleStrictRightBigram);
+		
 		set.add(singleFuzzyUnigram);
 		set.add(singleFuzzyLeftBigram);
 		set.add(singleFuzzyRightBigram);
+		set.add(doubleFuzzyLeftBigram);
+		set.add(doubleFuzzyRightBigram);
 
 		return new FuzzyBigramWithFallbackReducer().reduce(set);
 	}
