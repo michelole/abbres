24919ea146f1b342a14dad985d51772f97e3a2f6
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2016-12-18 19:03:44 -0200

    Adds tricky cases of relative gains

--- src/test/java/at/medunigraz/imi/abbres/model/AbbreviationTest.java
+++ src/test/java/at/medunigraz/imi/abbres/model/AbbreviationTest.java
@@ -18,12 +18,14 @@ public class AbbreviationTest extends TestCase {
 		assertTrue((new Abbreviation("intensiv.")).isValidExpansion("intensives")); // 0.25
 		assertTrue((new Abbreviation("ret.")).isValidExpansion("retard")); // 1.00
 		assertTrue((new Abbreviation("u.")).isValidExpansion("und")); // 2.00
+		assertTrue((new Abbreviation("p.")).isValidExpansion("post"));	// 3.00
 		assertTrue((new Abbreviation("rez.")).isValidExpansion("rezidivierende")); // 3.67
 		assertTrue((new Abbreviation("ms.")).isValidExpansion("Millisekunden")); // 5.50
 
 		//assertFalse((new Abbreviation("ret.")).isValidExpansion("retrosternales")); // 3.67
 
 		// Extreme gains, not valid
+		//assertTrue((new Abbreviation("Z.")).isValidExpansion("Zustand")); // 6.00
 		assertFalse((new Abbreviation("p.")).isValidExpansion("propter")); // 6.00
 		assertFalse((new Abbreviation("li.")).isValidExpansion("lipidsenkenden")); // 6.00
 		assertFalse((new Abbreviation("re.")).isValidExpansion("rechtsventrikulär")); // 7.50
